d72bdc7dc60f8804e12775a5169d0b3ef4771670
titancx <405975619@qq.com>
2018-02-06 17:25:56 +0800

    first

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="recommend">
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+  recommend
+    background-color:red
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="singer">
+    singer
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
